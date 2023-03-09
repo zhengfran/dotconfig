80d76f13610ac7794e9abb9168e367d3937c64bd
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2023-03-09 08:53:48 +0800

    add common environmental vaiables

---
+++ zsh/env.zsh
@@ -0,0 +1,6 @@
+export PATH=$HOME/.local/bin:$PATH
+export PATH=/usr/local/bin:$PATH
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export TERM=screen-256color-bce
