25d948d857ff7a78efac99b98b0838506ef471d4
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2023-03-15 09:04:40 +0800

    add klocwork directory to path on build server in work

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,3 +4,7 @@ export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 export TERM=screen-256color-bce
+if [[ $(hostname) == "IGAS116X" ]]; then
+    export PATH=$HOME/tooling/klocwork/user64/bin:$PATH
+fi
+
