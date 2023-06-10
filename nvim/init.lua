b470c51853f67dbd806aaf3a27881f1383964dc9
Zheng Zhicheng <zhicheng.ink98protonmail.com>
2023-06-11 03:13:50 +0800

    Refractor configuration using Lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,70 @@
-require("config_from_scratch.plugins-setup")
-require("config_from_scratch.core.options")
-require("config_from_scratch.core.keymaps")
-require("config_from_scratch.core.colorscheme")
-require("config_from_scratch.plugins.comment")
-require("config_from_scratch.plugins.nvim-tree")
-require("config_from_scratch.plugins.lualine")
-require("config_from_scratch.plugins.telescope")
-require("config_from_scratch.plugins.nvim-cmp")
-require("config_from_scratch.plugins.autopairs")
-require("config_from_scratch.plugins.autosave")
-require("config_from_scratch.plugins.bufferline")
-require("config_from_scratch.plugins.treesitter")
-require("config_from_scratch.plugins.gitsigns")
-require("config_from_scratch.plugins.osc52")
-require("config_from_scratch.plugins.which-key")
-require("config_from_scratch.plugins.toggleterm")
-require("config_from_scratch.plugins.gitblame")
-require("config_from_scratch.plugins.lsp.mason")
-require("config_from_scratch.plugins.lsp.lspsaga")
-require("config_from_scratch.plugins.lsp.lspconfig")
-require("config_from_scratch.plugins.lsp.null-ls")
+local opt = vim.opt --for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+--clipboard
+opt.clipboard:append("unnamedplus")
+
+--split windows
+opt.splitright = true
+opt.splitbelow = true
+
+--disable swapfile and backup
+opt.swapfile = false
+opt.backup = false
+--make "-" part of a word
+opt.iskeyword:append("_")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+--general keymaps
+vim.keymap.set("i", "fd", "<ESC>")
+
+-- remove search highlight
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- x does not copy character into register
+vim.keymap.set("n", "x", '"_x')
+
+require ('lazy').setup('plugins')
+
--- nvim/lua/config_from_scratch/plugins/which-key.lua
+++
@@ -1,6 +0,0 @@
-local setup, which_key = pcall(require, "which-key")
-if not setup then
-    return
-end
-
-which_key.setup()
