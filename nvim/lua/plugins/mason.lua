64f4e8592d3a90a7fa683ac85c8bc38c2096a41e
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2023-06-11 03:13:50 +0800

    Refractor configuration using Lazy

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,34 @@
+return {
+	-- lsp servers , linters, and formatters
+	"williamboman/mason.nvim",
+
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
+	},
+
+	lazy = false,
+
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"clangd",
+				"lua_ls",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		require("mason-null-ls").setup({
+			-- list of formatters & linters for mason to install
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"clang_format", -- c/cpp formatter
+			},
+			-- auto-install configured formatters & linters (with null-ls)
+			automafic_installation = true,
+		})
+	end,
+}
