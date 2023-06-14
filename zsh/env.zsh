aaa9b30d63b8b5e60ac9cea2f1ecd9824e54606c
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2023-06-14 14:59:11 +0800

    temporarily use environmental variable for OPNAI_API_KEY

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,6 +3,7 @@ export PATH=/usr/local/bin:$PATH
 export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export OPENAI_API_KEY="$(cat ~/.netrc | awk '{print $NF}')"
 export TERM=screen-256color-bce
 if [[ $(hostname) == "IGAS116X" ]]; then
     export PATH=$HOME/tooling/klocwork/user64/bin:$PATH
