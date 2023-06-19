5e4d451726465fa4fcd64cca88cbde8db192c64f
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2023-06-19 11:10:04 +0800

    Add rust env

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,5 @@
 export PATH=$HOME/.local/bin:$PATH
-export PATH=/usr/local/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
 export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
