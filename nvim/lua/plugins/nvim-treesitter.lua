b1e5353238db754cc205a2d6d86032ac7d860dde
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-01-04 08:38:52 +0800

    update nvim config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,36 +4,37 @@ return {
 		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 		ts_update()
 	end,
-    dependencies = {
-        { "windwp/nvim-ts-autotag"}, -- autoclose tags
-    },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-        	-- enable syntax highlighting
-        	highlight = {
-        		enable = true,
-        	},
-        	-- enable indentation
-        	indent = { enable = true },
-        	-- enable autotagging (w/ nvim-ts-autotag plugin)
-        	autotag = { enable = true },
-        	-- ensure these language parsers are installed
-        	ensure_installed = {
-        		"c",
-        		"cpp",
-        		"json",
-        		"yaml",
-        		"markdown",
-        		"markdown_inline",
-        		"bash",
-        		"lua",
-        		"vim",
-        		"dockerfile",
-        		"gitignore",
-        	},
-        	-- auto install above language parsers
-        	auto_install = true,
-        })
-    
-    end
+	dependencies = {
+		{ "windwp/nvim-ts-autotag" }, -- autoclose tags
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = { enable = true },
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"c",
+				"cpp",
+				"json",
+				"yaml",
+				"xml",
+				"groovy",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+			},
+			-- auto install above language parsers
+			auto_install = true,
+		})
+	end,
 }
