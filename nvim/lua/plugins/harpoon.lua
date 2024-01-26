73b3638edc822c6f6e953b6cefcf36bf578c5cda
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2024-01-26 13:31:09 +0800

    change harpoon size and add ways to quit toggle term

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,11 @@ return {
 	lazy = true, -- it lazyload itself
 	config = function()
 		local harpoon = require("harpoon")
-		harpoon.setup({})
+		harpoon.setup({
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 4,
+			},
+		})
 		vim.keymap.set("n", "<leader>hm", ":lua require('harpoon.mark').add_file()<CR>") -- mark file in harpoon
 		vim.keymap.set("n", "<leader>hq", ":lua require('harpoon.ui').toggle_quick_menu()<CR>") -- mark file in harpoon
 	end,
