4467f6cc2ae1a568301ca186b336b766bc75f30d
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-01-26 12:14:29 +0800

    Add harpoon and remove autosave

--- nvim/lua/plugins/telescope-vim-bookmarks.lua
+++ nvim/lua/plugins/telescope-vim-bookmarks.lua
@@ -1,6 +1,10 @@
 return {
-    "tom-anders/telescope-vim-bookmarks.nvim",
-    dependencies = {
-        "MattesGroeger/vim-bookmarks",
-    }
+	"tom-anders/telescope-vim-bookmarks.nvim",
+	dependencies = {
+		"MattesGroeger/vim-bookmarks",
+	},
+	config = function()
+		vim.g.bookmark_save_per_working_dir = 1
+		vim.g.bookmark_auto_save = 1
+	end,
 }
