9dd9cd1c24000122c60c604532c5542904cb85d6
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2024-02-26 13:15:44 +0800

    Fix clangd and lualine minor issues

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -23,7 +23,7 @@ return {
 					{
 						"filename",
 						file_status = true, -- displays file status (readonly status, modified status)
-						path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+						path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
 					},
 				},
 			},
