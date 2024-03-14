b6c8313434356832f04f5273f896f4266494581a
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2024-03-14 13:29:10 +0800

    remove rustfmt

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,38 +1,37 @@
 return {
-    -- lsp servers , linters, and formatters
-    "williamboman/mason.nvim",
+	-- lsp servers , linters, and formatters
+	"williamboman/mason.nvim",
 
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
-        "nvimtools/none-ls.nvim", -- bridges gap b/w mason & null-ls
-    },
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
+		"nvimtools/none-ls.nvim", -- bridges gap b/w mason & null-ls
+	},
 
-    lazy = false,
+	lazy = false,
 
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "clangd",
-                "lua_ls",
-                "jsonls",
-                "dockerls",
-                "rust_analyzer",
-                "pyright",
-                "cmake",
-            },
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
-        })
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"clangd",
+				"lua_ls",
+				"jsonls",
+				"dockerls",
+				"rust_analyzer",
+				"pyright",
+				"cmake",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
 		require("mason-null-ls").setup({
 			-- list of formatters & linters for mason to install
 			ensure_installed = {
 				"stylua", -- lua formatter
 				"clang_format", -- c/cpp formatter
-				"rustfmt", -- rust formatter
 				"jq", -- json formatter
 				"cmakelang", -- cmake formatter
 			},
