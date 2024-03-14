3b7ce204acbfec1466e0e5e1aab17d0c7e855b48
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2024-03-14 13:22:46 +0800

    highlight text when yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,6 +41,15 @@ opt.backup = false
 --make "-" part of a word
 opt.iskeyword:append("_")
 
+-- highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
