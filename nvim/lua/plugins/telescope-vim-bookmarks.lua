ab3e3c3bc23d43a87246ff7d07205a29feaa50df
Zhicheng Zheng <zhicheng.zheng@continental-corporation.com>
2024-05-01 14:44:55 +0800

    update config

--- nvim/lua/plugins/telescope-vim-bookmarks.lua
+++ nvim/lua/plugins/telescope-vim-bookmarks.lua
@@ -4,7 +4,8 @@ return {
 		"MattesGroeger/vim-bookmarks",
 	},
 	config = function()
-		vim.g.bookmark_save_per_working_dir = 1
+		-- vim.g.bookmark_save_per_working_dir = 1
 		vim.g.bookmark_auto_save = 1
+		vim.g.bookmark_highlight_lines = 1
 	end,
 }
