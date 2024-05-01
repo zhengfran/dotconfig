cef29b1352756bca9d7e4268ff154656ddf83d98
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-05-01 14:44:55 +0800

    update config

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -34,7 +34,6 @@ return {
 					"<tab>",
 					function()
 						return require("luasnip").expand_or_locally_jumpable() and "<Plug>luasnip-jump-next"
-						-- or "<c-\\><c-n>:call searchpair('[([{<|]', '', '[)\\]}>|]', 'W')<cr>a"
 					end,
 					expr = true,
 					silent = true,
