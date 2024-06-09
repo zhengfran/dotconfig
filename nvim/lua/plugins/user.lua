b0c9efd66e2f32559f2e298b5feb37a3f02e79b6
uie41443(Zheng Zhicheng) <zhicheng.zheng@continental-corporation.com>
2024-06-09 11:17:30 +0800

    remove setting which conflict with noice

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -5,12 +5,12 @@
 return {
 
   -- == Examples of Adding Plugins ==
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
