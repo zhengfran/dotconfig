6ed1ec2985a01bbb9bd533bfa7c8e7a20dc12c0a
uie41443(Zheng Zhicheng) <zhicheng.zheng@continental-corporation.com>
2024-06-12 15:48:30 +0800

    add windows powershell config with starship

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+Import-Module ZLocation
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# auto suggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
+Set-Alias -Name ll -Value ls
+Set-Alias -Name lg -Value lazygit
+Set-Alias -Name c -Value clear
+Set-Alias -Name p -Value pwd
+Set-Alias -Name nm -Value neovim
+Set-Alias -Name vi -Value neovide
+
+# 让powershell 支持linux 下的快捷键
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+    Set-PSReadLineOption -EditMode Emacs
+}
+# starship
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\dotconfig\starship\starship.toml"
