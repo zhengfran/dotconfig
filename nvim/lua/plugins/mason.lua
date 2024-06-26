04334a3d3eb7552d00a65caf35b26e9ba81ca83c
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-06-26 17:16:42 +0800

    remove black as python formatter

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -25,7 +25,7 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
-        "black", --python Formatter
+        -- "black", --python Formatter
         -- add more arguments for adding more null-ls sources
       },
     },
