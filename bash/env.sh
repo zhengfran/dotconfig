3ebfcbc2bf2eca57373a904f2aa1d7ad98c8fe0b
root <root@IGL3286W.cw01.contiwan.com>
2024-07-14 14:26:39 +0800

    nvim and bash config update

--- bash/env.sh
+++ bash/env.sh
@@ -5,7 +5,7 @@ export PATH=$HOME/bin/nvim-linux64/bin:$PATH
 export PATH=$HOME/bin/node/node/bin:$PATH
 export PATH=$HOME/bin/clang-14/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.config/emacs/bin:$PATH
 
 export EDITOR=nvim
-export OPENAI_API_KEY="$(cat ~/.netrc | awk '{print $NF}')"
 export TERM=screen-256color-bce
