94d2dfeb4a0690c169a670bd4b0d44568ebcb074
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-12-24 15:15:41 +0800

    nvim rework from scratch

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,59 @@
+vim.g.mapleader = " "
+
+local mode_nv = { "n", "v" }
+local mode_v = { "v" }
+local mode_i = { "i" }
+local nmappings = {
+	{ from = "<c-s>",             to = ":w<CR>" },
+	{ from = "<c-q>",             to = ":q<CR>" },
+	{ from = ";",             to = ":",                                                                   mode = mode_nv },
+	{ from = "Y",             to = "\"+y",                                                                mode = mode_v },
+	{ from = "`",             to = "~",                                                                   mode = mode_nv },
+
+	-- Useful actions
+	{ from = ",.",            to = "%",                                                                   mode = mode_nv },
+	{ from = "<c-y>",         to = "<ESC>A {}<ESC>i<CR><ESC>ko",                                          mode = mode_i },
+	{ from = "\\v",           to = "v$h", },
+	{ from = "<c-a>",         to = "<ESC>A",                                                              mode = mode_i },
+	{ from = "kj",            to = "<ESC>",                                                              mode = mode_i },
+
+	-- Tab management
+	{ from = "to",            to = "<cmd>tabnew<CR>", },
+	{ from = "tx",            to = "<cmd>tabclose<CR>", },
+	{ from = "tn",            to = "<cmd>tabn<CR>", },
+	{ from = "tp",            to = "<cmd>tabp<CR>", },
+
+	-- Other
+	{ from = "<leader>sw",    to = ":set wrap<CR>" },
+	{ from = "<leader>sc",    to = ":set spell!<CR>" },
+	{ from = "<leader><CR>",  to = ":nohlsearch<CR>" },
+	{ from = "<f10>",         to = ":TSHighlightCapturesUnderCursor<CR>" },
+	{ from = "<leader>o",     to = "za" },
+	{ from = "<leader>pr",    to = ":profile start profile.log<CR>:profile func *<CR>:profile file *<CR>" },
+	{ from = "<leader>rc",    to = ":e ~/.config/nvim/init.lua<CR>" },
+	{ from = "<leader>rv",    to = ":e .vim.lua<CR>" },
+	{ from = ",v",            to = "v%" },
+	{ from = "<leader><esc>", to = "<nop>" },
+
+}
+
+vim.keymap.set("n", "q", "<nop>", { noremap = true })
+vim.keymap.set("n", ",q", "q", { noremap = true })
+
+for _, mapping in ipairs(nmappings) do
+	vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, { noremap = true })
+end
+
+local function run_vim_shortcut(shortcut)
+	local escaped_shortcut = vim.api.nvim_replace_termcodes(shortcut, true, false, true)
+	vim.api.nvim_feedkeys(escaped_shortcut, 'n', true)
+end
+
+-- close win below
+vim.keymap.set("n", "<leader>q", function()
+	require("trouble").close()
+	local wins = vim.api.nvim_tabpage_list_wins(0)
+	if #wins > 1 then
+		run_vim_shortcut([[<C-w>j:q<CR>]])
+	end
+end, { noremap = true, silent = true })
