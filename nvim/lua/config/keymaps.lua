97df00ef388d2efa3934e1c8885671f73a66d03a
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-12-24 15:56:45 +0800

    minor fix

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,8 +4,12 @@ local mode_nv = { "n", "v" }
 local mode_v = { "v" }
 local mode_i = { "i" }
 local nmappings = {
-	{ from = "<c-s>",             to = ":w<CR>" },
-	{ from = "<c-q>",             to = ":q<CR>" },
+	{ from = "<c-s>",         to = ":w<CR>",                                                              mode = mode_n },
+	{ from = "<c-s>",         to = "<C-o>:w<CR>",                                                         mode = mode_i },
+	{ from = "<c-s>",         to = "<Esc>:w<CR>",                                                         mode = mode_v },
+	{ from = "<c-q>",         to = ":q<CR>",                                                              mode = mode_n },
+	{ from = "<c-q>",         to = "<C-o>:q<CR>",                                                         mode = mode_i },
+	{ from = "<c-q>",         to = "<Esc>:q<CR>",                                                         mode = mode_v },
 	{ from = ";",             to = ":",                                                                   mode = mode_nv },
 	{ from = "Y",             to = "\"+y",                                                                mode = mode_v },
 	{ from = "`",             to = "~",                                                                   mode = mode_nv },
@@ -15,7 +19,7 @@ local nmappings = {
 	{ from = "<c-y>",         to = "<ESC>A {}<ESC>i<CR><ESC>ko",                                          mode = mode_i },
 	{ from = "\\v",           to = "v$h", },
 	{ from = "<c-a>",         to = "<ESC>A",                                                              mode = mode_i },
-	{ from = "kj",            to = "<ESC>",                                                              mode = mode_i },
+	{ from = "kj",            to = "<ESC>",                                                               mode = mode_i },
 
 	-- Tab management
 	{ from = "to",            to = "<cmd>tabnew<CR>", },
