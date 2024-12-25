ccccb59d354349ff1d39b82838235665b75a3b79
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-12-25 21:04:57 +0800

    yazi update and integrated with powershell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -24,3 +24,13 @@ if ($host.Name -eq 'ConsoleHost')
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
 }
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
