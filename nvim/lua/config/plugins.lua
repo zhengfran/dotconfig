7a9662755f81074204e34b26e510494b181de28d
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2024-12-30 08:49:31 +0800

    remove lazy keymaps

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -11,21 +11,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_cmd = require("lazy.view.config").commands
-local lazy_keys = {
-	{ cmd = "install", key = "i" },
-	{ cmd = "update",  key = "u" },
-	{ cmd = "sync",    key = "s" },
-	{ cmd = "clean",   key = "cl" },
-	{ cmd = "check",   key = "ch" },
-	{ cmd = "log",     key = "l" },
-	{ cmd = "restore", key = "rs" },
-	{ cmd = "profile", key = "p" },
-}
-for _, v in ipairs(lazy_keys) do
-	lazy_cmd[v.cmd].key = "<SPC>" .. v.key
-	lazy_cmd[v.cmd].key_plugin = "<leader>" .. v.key
-end
+-- local lazy_cmd = require("lazy.view.config").commands
+-- local lazy_keys = {
+-- 	{ cmd = "install", key = "i" },
+-- 	{ cmd = "update",  key = "u" },
+-- 	{ cmd = "sync",    key = "s" },
+-- 	{ cmd = "clean",   key = "cl" },
+-- 	{ cmd = "check",   key = "ch" },
+-- 	{ cmd = "log",     key = "l" },
+-- 	{ cmd = "restore", key = "rs" },
+-- 	{ cmd = "profile", key = "p" },
+-- }
+-- for _, v in ipairs(lazy_keys) do
+-- 	lazy_cmd[v.cmd].key = "<SPC>" .. v.key
+-- 	lazy_cmd[v.cmd].key_plugin = "<leader>" .. v.key
+-- end
 vim.keymap.set("n", "<leader>pl", ":Lazy<CR>", { noremap = true })
 
 require("lazy").setup({
