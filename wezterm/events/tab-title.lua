440830dc00ee779964fc10b264201113f42dc1b6
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2025-03-01 10:23:40 +0800

    wezterm shortcut refinement

--- wezterm/events/tab-title.lua
+++ wezterm/events/tab-title.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 -- Inspired by https://github.com/wez/wezterm/discussions/628#discussioncomment-1874614
 
@@ -21,33 +21,33 @@ local colors = {
 }
 
 local _set_process_name = function(s)
-   local a = string.gsub(s, '(.*[/\\])(.*)', '%2')
-   return a:gsub('%.exe$', '')
+  local a = string.gsub(s, "(.*[/\\])(.*)", "%2")
+  return a:gsub("%.exe$", "")
 end
 
 local _set_title = function(process_name, base_title, max_width, inset)
-   local title
-   inset = inset or 6
+  local title
+  inset = inset or 6
 
-   if process_name:len() > 0 then
-      title = process_name .. ' ~ ' .. base_title
-   else
-      title = base_title
-   end
+  if process_name:len() > 0 then
+    title = process_name .. " ~ " .. base_title
+  else
+    title = base_title
+  end
 
-   if title:len() > max_width - inset then
-      local diff = title:len() - max_width + inset
-      title = wezterm.truncate_right(title, title:len() - diff)
-   end
+  if title:len() > max_width - inset then
+    local diff = title:len() - max_width + inset
+    title = wezterm.truncate_right(title, title:len() - diff)
+  end
 
-   return title
+  return title
 end
 
 local _check_if_admin = function(p)
-   if p:match('^Administrator: ') then
-      return true
-   end
-   return false
+  if p:match("^Administrator: ") then
+    return true
+  end
+  return false
 end
 
 ---@param fg string
@@ -55,65 +55,69 @@ end
 ---@param attribute table
 ---@param text string
 local _push = function(bg, fg, attribute, text)
-   table.insert(__cells__, { Background = { Color = bg } })
-   table.insert(__cells__, { Foreground = { Color = fg } })
-   table.insert(__cells__, { Attribute = attribute })
-   table.insert(__cells__, { Text = text })
+  table.insert(__cells__, { Background = { Color = bg } })
+  table.insert(__cells__, { Foreground = { Color = fg } })
+  table.insert(__cells__, { Attribute = attribute })
+  table.insert(__cells__, { Text = text })
 end
 
 M.setup = function()
-   wezterm.on('format-tab-title', function(tab, _tabs, _panes, _config, hover, max_width)
-      __cells__ = {}
+  wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, hover, max_width)
+    __cells__ = {}
 
-      local bg
-      local fg
-      local process_name = _set_process_name(tab.active_pane.foreground_process_name)
-      local is_admin = _check_if_admin(tab.active_pane.title)
-      local title = _set_title(process_name, tab.active_pane.title, max_width, (is_admin and 8))
+    local bg
+    local fg
+    local process_name = _set_process_name(tab.active_pane.foreground_process_name)
+    local is_admin = _check_if_admin(tab.active_pane.title)
+    local index = tab.tab_index + 1 -- Convert zero-based index to one-based
+    local title = _set_title(process_name, tab.active_pane.title, max_width, (is_admin and 8))
 
-      if tab.is_active then
-         bg = colors.is_active.bg
-         fg = colors.is_active.fg
-      elseif hover then
-         bg = colors.hover.bg
-         fg = colors.hover.fg
-      else
-         bg = colors.default.bg
-         fg = colors.default.fg
+    -- Prepend tab number to title
+    title = string.format("%d: %s", index, title)
+
+    if tab.is_active then
+      bg = colors.is_active.bg
+      fg = colors.is_active.fg
+    elseif hover then
+      bg = colors.hover.bg
+      fg = colors.hover.fg
+    else
+      bg = colors.default.bg
+      fg = colors.default.fg
+    end
+
+    local has_unseen_output = false
+    for _, pane in ipairs(tab.panes) do
+      if pane.has_unseen_output then
+        has_unseen_output = true
+        break
       end
+    end
 
-      local has_unseen_output = false
-      for _, pane in ipairs(tab.panes) do
-         if pane.has_unseen_output then
-            has_unseen_output = true
-            break
-         end
-      end
+    -- Left semi-circle
+    _push("rgba(0, 0, 0, 0.4)", bg, { Intensity = "Bold" }, GLYPH_SEMI_CIRCLE_LEFT)
 
-      -- Left semi-circle
-      _push('rgba(0, 0, 0, 0.4)', bg, { Intensity = 'Bold' }, GLYPH_SEMI_CIRCLE_LEFT)
+    -- Admin Icon
+    if is_admin then
+      _push(bg, fg, { Intensity = "Bold" }, " " .. GLYPH_ADMIN)
+    end
 
-      -- Admin Icon
-      if is_admin then
-         _push(bg, fg, { Intensity = 'Bold' }, ' ' .. GLYPH_ADMIN)
-      end
+    -- Title (with tab number)
+    _push(bg, fg, { Intensity = "Bold" }, " " .. title)
 
-      -- Title
-      _push(bg, fg, { Intensity = 'Bold' }, ' ' .. title)
+    -- Unseen output alert
+    if has_unseen_output then
+      _push(bg, "#FFA066", { Intensity = "Bold" }, " " .. GLYPH_CIRCLE)
+    end
 
-      -- Unseen output alert
-      if has_unseen_output then
-         _push(bg, '#FFA066', { Intensity = 'Bold' }, ' ' .. GLYPH_CIRCLE)
-      end
+    -- Right padding
+    _push(bg, fg, { Intensity = "Bold" }, " ")
 
-      -- Right padding
-      _push(bg, fg, { Intensity = 'Bold' }, ' ')
+    -- Right semi-circle
+    _push("rgba(0, 0, 0, 0.4)", bg, { Intensity = "Bold" }, GLYPH_SEMI_CIRCLE_RIGHT)
 
-      -- Right semi-circle
-      _push('rgba(0, 0, 0, 0.4)', bg, { Intensity = 'Bold' }, GLYPH_SEMI_CIRCLE_RIGHT)
-
-      return __cells__
-   end)
+    return __cells__
+  end)
 end
 
 return M
