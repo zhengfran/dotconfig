94ebd78f6c8219787e153ca2b5fb452f4e617d3d
Zheng Zhicheng (uie41442) <zhicheng.zheng@continental-corporation.com>
2025-03-05 09:46:59 +0800

    tmux vim navigator config simplify

--- nvim/lazy/lua/plugins/tmux.lua
+++ nvim/lazy/lua/plugins/tmux.lua
@@ -1,17 +1,10 @@
 return {
   "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
   keys = {
-    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to the previous pane" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Got to the left pane" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Got to the down pane" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Got to the up pane" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Got to the right pane" },
   },
 }
