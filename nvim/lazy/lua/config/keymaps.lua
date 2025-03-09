dcfeae5d63350a5c233405a6faf265ad1f59e214
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2025-03-09 16:30:56 +0800

    some nvim config linting

--- nvim/lazy/lua/config/keymaps.lua
+++ nvim/lazy/lua/config/keymaps.lua
@@ -6,10 +6,10 @@ local mode_i = { "i" }
 local mode_v = { "v" }
 -- local mode_nv = { "n", "v" }
 local nmappings = {
-  { from = "<c-q>", to = ":q<CR>", mode = mode_n },
-  { from = "<c-q>", to = "<C-o>:q<CR>", mode = mode_i },
-  { from = "jj", to = "<Esc>", mode = mode_i },
-  { from = "<c-q>", to = "<Esc>:q<CR>", mode = mode_v },
+  { from = "<c-q>",      to = ":q<CR>",          mode = mode_n },
+  { from = "<c-q>",      to = "<C-o>:q<CR>",     mode = mode_i },
+  { from = "jj",         to = "<Esc>",           mode = mode_i },
+  { from = "<c-q>",      to = "<Esc>:q<CR>",     mode = mode_v },
   { from = "<leader>qf", to = "<cmd>cclose<CR>", mode = mode_n },
 }
 
