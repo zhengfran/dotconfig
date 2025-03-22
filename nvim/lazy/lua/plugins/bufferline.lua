ceb1675f50f59966f52a48f18ad7072597e9acb8
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2025-03-22 11:52:16 +0800

    another mac update

--- nvim/lazy/lua/plugins/bufferline.lua
+++ nvim/lazy/lua/plugins/bufferline.lua
@@ -6,11 +6,6 @@ return {
     { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
-    {
-      "<leader>bt",
-      "<Cmd>BufferLineGroupClose<CR>",
-      desc = "toggle BufferLine Groups",
-    },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
     { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
@@ -25,49 +20,6 @@ return {
       -- stylua: ignore
       right_mouse_command = function(n) Snacks.bufdelete(n) end,
       diagnostics = "nvim_lsp",
-      groups = {
-        items = {
-          {
-            name = "Code",
-            matcher = function(buf)
-              return buf.name:match("%.c$")
-                or buf.name:match("%.rs$")
-                or buf.name:match("%.lua$")
-                or buf.name:match("%.h$")
-            end,
-          },
-          {
-            name = "Docs",
-            matcher = function(buf)
-              return buf.name:match("%.md$") or buf.name:match("%.org$") or buf.name:match("%.txt$")
-            end,
-          },
-          {
-            name = "Debug",
-            matcher = function(buf)
-              return buf.name:match("%.t32$") or buf.name:match("%.cmm$")
-            end,
-          },
-          {
-            name = "Script",
-            matcher = function(buf)
-              return buf.name:match("%.sh$") or buf.name:match("%.bat$") or buf.name:match("%.ps1$")
-            end,
-          },
-          {
-            name = "Config",
-            matcher = function(buf)
-              return buf.name:match("%.json$") or buf.name:match("%.toml$") or buf.name:match("%.conf$")
-            end,
-          },
-          {
-            name = "Log",
-            matcher = function(buf)
-              return buf.name:match("%.txt$") or buf.name:match("%.log$")
-            end,
-          },
-        },
-      },
       always_show_bufferline = false,
       diagnostics_indicator = function(_, _, diag)
         local icons = LazyVim.config.icons.diagnostics
