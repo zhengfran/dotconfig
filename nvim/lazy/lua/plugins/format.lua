498951f427f619324f22138c6b45223d5fed871b
Zheng Zhicheng <zhicheng.ink98@gmail.com>
2025-04-06 16:15:55 +0800

    add json formater and show hidden files when find

--- nvim/lazy/lua/plugins/format.lua
+++ nvim/lazy/lua/plugins/format.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         fish = { "fish_indent" },
+        json = { "prettier" },
         sh = { "shfmt" },
         html = { "prettier" },
         rust = { "rustfmt" },
