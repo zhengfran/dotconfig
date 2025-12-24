6d704fe8387a9a3be4924b9fca67400a83290baa
uie41442 <zhicheng.zheng@aumovio.com>
2025-12-24 17:11:02 +0800

    create setup scripts

---
+++ scripts/setup-packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install debian/ubuntu packages
+sudo apt install -y git wl-clipboard zsh ffmpeg 7zip jq poppler-utils zoxide imagemagick
+
+# Install Homebrew if not present, certain debian packages are outdated
+if ! command -v brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+# Install packages only if not present
+for pkg in fzf fd ripgrep yazi lazygit nvim zenity; do
+    if ! brew list --formula | grep -qw "$pkg"; then
+        brew install "$pkg"
+    fi
+done
