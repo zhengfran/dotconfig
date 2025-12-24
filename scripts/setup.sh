20b2fab66d5f82acffdc18f0fac21bef1a5b2e62
uie41442 <zhicheng.zheng@aumovio.com>
2025-12-24 17:10:52 +0800

    remove lazy nvim config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,11 @@
 
-# Fetch config and set them up
-git clone -b Rebirth git@github.com:zhengfran/dotconfig.git
-##installtion
+# Fetch config if not present and set them up
+if [ ! -d ~/dotconfig ]; then
+    git clone -b Rebirth git@github.com:zhengfran/dotconfig.git ~/dotconfig
+fi
+# Symlink config files
+
+##installtions
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install zsh fzf fd ripgrep yazi lazygit
