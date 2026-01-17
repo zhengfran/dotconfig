f083a8c338b8388241de63b4e8e39f55e3a66f1e
uie41442 <zhicheng.zheng@aumovio.com>
2026-01-17 13:28:59 +0800

    update git ignore and delte chemacs

--- basic/editor/emacs/chemacs/.emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.config/emacs/default")))
- ("doom" . ((user-emacs-directory . "~/.config/doomemacs")
-            (env . (("DOOMDIR" . "~/.config/emacs/doom"))))))
-
